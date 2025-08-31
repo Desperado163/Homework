7fafe5ac2fb91bd1671e137cb838776ceaa40a68
Desperado163 <dmitriy.krylov763@yandex.ru>
2025-08-31 11:55:29 +0400

    Home building done in js

--- v1/hw9/hw9.js
+++ v1/hw9/hw9.js
@@ -8,8 +8,11 @@ console.log(titleEl);
 console.log(hideButtonEl);
 
 hideButtonEl.addEventListener('click', function () {
-    titleEl.textContent = '';
-    titleEl.textContent = 'Домашнее задание №9';
+    if (titleEl.style.display === 'none') {
+        titleEl.style.display = 'block';
+    } else {
+        titleEl.style.display = 'none';
+    }
 });
 
 console.log('\n%c Задание №2:', 'color:lightgreen');
@@ -23,3 +26,50 @@ colorChangeButtonEl.addEventListener('click', function() {
     textEl.style.color = 'blue';
 });
 
+console.log('\n%c Задание №3:', 'color:lightgreen');
+const textchangeButtonEl = document.querySelector('.textchangeButton');
+
+console.log(titleEl);
+console.log(textchangeButtonEl);
+
+textchangeButtonEl.addEventListener('click', () => {
+    titleEl.textContent = 'Привет, мир!';
+});
+
+console.log('\n%c Задание №4:', 'color:lightgreen');
+const AllDescriptionEl = document.querySelectorAll('.description');
+
+console.log(AllDescriptionEl);
+AllDescriptionEl.forEach(description => {
+    description.textContent = 'Измененный текст';
+});
+
+console.log('\n%c Задание №5:', 'color:lightgreen');
+const AllDescription5El = document.querySelectorAll('.description5');
+console.log(AllDescription5El);
+
+AllDescription5El.forEach(description5 => {
+    description5.textContent = 'Новый текст';
+});
+
+console.log('\n%c Задание №6:', 'color:lightgreen');
+const createsNewElementButtonEl = document.querySelector('.createsNewElementButton');
+
+console.log(createsNewElementButtonEl);
+
+createsNewElementButtonEl.addEventListener('click', () => {
+    const newParagraph = document.createElement('p');
+    newParagraph.textContent = 'Новый абзац';
+    document.body.appendChild(newParagraph);
+});
+
+console.log('\n%c Задание №7:', 'color:lightgreen');
+const elementDeletingButtonEl = document.querySelector('.elementDeletingButton');
+const description7El = document.querySelector('.description7');
+
+console.log(description7El);
+console.log(elementDeletingButtonEl);
+
+elementDeletingButtonEl.addEventListener('click', () => {
+    description7El.remove();
+});
